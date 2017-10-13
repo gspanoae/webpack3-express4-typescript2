651691bfeb301c8b8c6374de95584cf8836f0bda
Gabi Spanoae <gabi_spanoae@slc.co.uk>
2017-10-13 16:59:09 +0100

    basic setup

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# webpack3-express4-typescript2
-Starter project for Express 4 Applications using TypeScript 2 and Webpack 3
+### Starter project for Express 4 Applications using TypeScript 2 and Webpack 3
+**Setup**
+---
+**[Clone this Repository](https://github.com/gspanoae/webpack3-express4-typescript2.git)**
+**Install and build**
+```
+npm install
+```
+
+**Start Application**
+---
+```
+npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+//const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+var ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin'); //installed via npm
+const webpack = require('webpack'); //to access built-in plugins
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  target: 'node',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+      // Add `.ts` and `.tsx` as a resolvable extension.
+      extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: 'raw-loader'
+      },
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: { transpileOnly: true }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new ForkTsCheckerWebpackPlugin({ checkSyntacticErrors: true })
+    //new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
+};
